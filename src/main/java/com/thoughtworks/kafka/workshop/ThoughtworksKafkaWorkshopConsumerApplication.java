fd7c999e914a01082d870ca821c1d10d5cd76d9c
akhil-ghatiki <akhil.ghatiki@gmail.com>
2022-07-11 20:26:36 +0530

    Initial commit

---
+++ src/main/java/com/thoughtworks/kafka/workshop/ThoughtworksKafkaWorkshopConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.kafka.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtworksKafkaWorkshopConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtworksKafkaWorkshopConsumerApplication.class, args);
+	}
+
+}
